90bd0f0cbcfabad2253b268c9fddb2806e6c6387
maatarahmed <160156991+maatarahmed@users.noreply.github.com>
2025-11-01 22:49:52 +0100

    traduction

--- GiftoriaStore/app/(shop)/shop-default/page.jsx
+++ GiftoriaStore/app/(shop)/shop-default/page.jsx
@@ -1,21 +1,22 @@
+"use client";
+
 import Footer1 from "@/components/footers/Footer1";
 import Header10 from "@/components/headers/Header10";
 import ShopDefault from "@/components/shop/ShopDefault";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
-export const metadata = {
-  title: "Product Default || Ecomus - Ultimate Nextjs Ecommerce Template",
-  description: "Ecomus - Ultimate Nextjs Ecommerce Template",
-};
 export default function page() {
+  const { t } = useTranslation();
+  
   return (
     <>
       <Header10 />
       <div className="tf-page-title">
         <div className="container-full">
-          <div className="heading text-center text-uppercase">New Arrival</div>
-          <p className="text-center text-2 text_black-2 mt_5 text-uppercase">
-            Shop through our latest selection of Fashion
+          <div className="heading text-center text-uppercase">{t('shop_page.our_products')}</div>
+          <p className="text-center text-2 text_black-2 mt_5 text-uppercase montserrat-regular">
+            {t('shop_page.shop_latest_selection')}
           </p>
         </div>
       </div>
