f3b46d9288b9f1d1fdb2d0f3cbf63a044814a907
maatarahmed <160156991+maatarahmed@users.noreply.github.com>
2025-11-26 01:13:11 +0100

    fixed  occasion baseurl error

--- GiftoriaStore/components/homes/home-5/Collection.jsx
+++ GiftoriaStore/components/homes/home-5/Collection.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { API_BASE_URL_WITH_API } from "@/utils/config";
+import { API_BASE_URL, API_BASE_URL_WITH_API } from "@/utils/config";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
 import { useTranslation } from "react-i18next";
