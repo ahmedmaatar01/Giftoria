f778aca1373567651082a30e26c5b69c954206ef
maatarahmed <160156991+maatarahmed@users.noreply.github.com>
2026-02-09 23:40:09 +0100

    nata

--- GiftoriaStore/app/layout.js
+++ GiftoriaStore/app/layout.js
@@ -9,9 +9,29 @@ import "photoswipe/dist/photoswipe.css";
 import "./globals.css";
 import "rc-slider/assets/index.css";
 import Context from "@/context/Context";
+import ErrorBoundary from "@/components/common/ErrorBoundary";
 import OptimizedScripts from "@/components/common/OptimizedScripts";
 import ScrollTop from "@/components/common/ScrollTop";
 import RtlToggle from "@/components/common/RtlToggle";
+
+// Lazy load all modals
+import {
+  LazyHomesModal,
+  LazyQuickView,
+  LazyQuickAdd,
+  LazyProductSidebar,
+  LazyShopCart,
+  LazyAskQuestion,
+  LazyDeliveryReturn,
+  LazyLogin,
+  LazyMobileMenu,
+  LazyRegister,
+  LazyResetPass,
+  LazySearchModal,
+  LazyToolbarBottom,
+  LazyToolbarShop,
+  LazyShareModal,
+} from "@/components/modals/LazyModals";
 
 export default function RootLayout({ children }) {
   const [isClient, setIsClient] = useState(false);
@@ -59,7 +79,9 @@ export default function RootLayout({ children }) {
         </div>
         
         <Context>
-          <div id="wrapper">{children}</div>
+          <ErrorBoundary componentName="Main App">
+            <div id="wrapper">{children}</div>
+          </ErrorBoundary>
           
           <RtlToggle />
           <ScrollTop />
@@ -67,7 +89,7 @@ export default function RootLayout({ children }) {
           
           {/* Lazy-loaded modals - only load when needed */}
           {isClient && (
-            <>
+            <ErrorBoundary componentName="Modals">
               <LazyHomesModal />
               <LazyQuickView />
               <LazyQuickAdd />
@@ -83,7 +105,7 @@ export default function RootLayout({ children }) {
               <LazyToolbarBottom />
               <LazyToolbarShop />
               <LazyShareModal />
-            </>
+            </ErrorBoundary>
           )}
         </Context>
       </body>
