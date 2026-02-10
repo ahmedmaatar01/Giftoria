7a6ccf5e65469011c18f1cda39893741229aa69f
maatarahmed <160156991+maatarahmed@users.noreply.github.com>
2026-02-10 21:43:08 +0100

    changed appels to script head

--- backend/resources/views/sadad/pay.blade.php
+++ backend/resources/views/sadad/pay.blade.php
@@ -7,9 +7,6 @@
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link href="https://sadadqa.com/jslib/cards.min.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="https://sadadqa.com/jslib/cards.min.js?v=59"></script>
-    <script type="text/javascript" src="https://sadadqa.com/jslib/sadadmain.js?v=59"></script>
 </head>
 <body>
     <div style="max-width: 800px; margin: 0 auto; padding: 20px;">
@@ -36,6 +33,10 @@
              style="min-height: 400px; border: 1px solid #ddd; padding: 20px;">
         </div>
     </div>
+
+    <link href="https://sadadqa.com/jslib/cards.min.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="https://sadadqa.com/jslib/cards.min.js?v=59"></script>
+    <script type="text/javascript" src="https://sadadqa.com/jslib/sadadmain.js?v=59"></script>
 
     <script>
         // SADAD initialization (from sadad.js)
