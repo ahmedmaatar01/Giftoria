121df04bbd51cde7b7d37e52200c16b31e783797
maatarahmed <160156991+maatarahmed@users.noreply.github.com>
2026-02-10 21:14:59 +0100

    jquery added

--- backend/resources/views/sadad/pay.blade.php
+++ backend/resources/views/sadad/pay.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SADAD Payment</title>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://sadadqa.com/jslib/sadad.js"></script>
 </head>
 <body>
